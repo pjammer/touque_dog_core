bcbd2b32c5c16682c4c797d642d93c9f0577dfa0
nerbie69 <philipjingram@yahoo.com>
2008-09-16 23:27:31 -0400

    changing file references

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,31 +3,31 @@ class UserMailer < ActionMailer::Base
     setup_email(user)
     @subject    += 'Please activate your new account'
   
-    @body[:url]  = "http://www.prettybycritty.com/activate/#{user.activation_code}"
+    @body[:url]  = "http://www.example.com/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://www.prettybycritty.com/#{user.login}"
+    @body[:url]  = "http://www.example.com/#{user.login}"
   end
   
   def forgot_password(user)
      setup_email(user)
      @subject    += 'You have requested to change your password'
-     @body[:url]  = "http://localhost:3000/reset_password/#{user.password_reset_code}"
+     @body[:url]  = "http://www.example.com/reset_password/#{user.password_reset_code}"
    end
 
    def reset_password(user)
      setup_email(user)
      @subject    += 'Your password has been reset.'
-     @body[:url] = "http://www.prettybycritty.com"
+     @body[:url] = "http://www.example.com"
    end
   
   def friend_request(mail)
-    subject 'New friend request at Prettybycritty.com'
-    from 'Prettybycritty <no-reply@prettybycritty.com>'
+    subject 'New friend request at example.com'
+    from 'example <no-reply@example.com>'
     recipients mail[:friend].email
     body mail    
   end
@@ -35,7 +35,7 @@ class UserMailer < ActionMailer::Base
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "noreply@prettybycritty.com"
+      @from        = "noreply@example.com"
       @subject     = ""
       @sent_on     = Time.now
       @body[:user] = user
